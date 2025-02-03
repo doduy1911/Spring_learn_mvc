5b2c4c32bfa090519adccf88b4de0480f9eb40a4
Your duy <you@example.com>
2025-02-03 16:58:53 +0700

    send data với html form

--- src/main/java/com/example/laptopshop/controller/UserController.java
+++ src/main/java/com/example/laptopshop/controller/UserController.java
@@ -1,17 +1,28 @@
 package com.example.laptopshop.controller;
 
+import com.example.laptopshop.domain.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+
 public class UserController {
     @GetMapping("/")
     public String getHomePage() {
         return "index";
     }
-    @GetMapping("/admin/user")
-    public String User(){
+    @RequestMapping("/admin/user")
+    public String User(Model model){
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1",method = RequestMethod.POST)
+    public String CreateUserPage(Model model,@ModelAttribute("newUser") User doduy){
+        System.out.println("aaaa"+doduy);
+
         return "create";
+
     }
 }
